3e61f9d599248ce7a09081271f5658b8f0f279e9
LB - RPI <lorenzobrutti80@gmail.com>
2021-06-29 17:12:23 +0100

    update confs

--- dashboard/src/MessageStack/config.js
+++ dashboard/src/MessageStack/config.js
@@ -1,7 +1,7 @@
 export default {
     // MQTT servers websocket connection
     port: 9001,
-    host: '127.0.0.1',
+    host: '192.168.1.32',
     username: 'dashboard',
     password: 'bledemo',
     clientId: 'bledemo_' + Math.random().toString(16).substr(2, 8),
